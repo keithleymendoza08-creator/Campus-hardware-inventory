7e4b6368aabeac910e8212d3c3972aaae7eaf00c
keithleymendoza08 <keithleymendoza08@gmail.com>
2026-09-24 01:03:40 +0800

    Initial web application

---
+++ Laboratorysystem.py
@@ -0,0 +1,9 @@
+try:
+    import tkinter as tk
+    from tkinter import messagebox, ttk
+    TKINTER_AVAILABLE = True
+except ImportError:
+    tk = None
+    messagebox = None
+    ttk = None
+    TKINTER_AVAILABLE = False
